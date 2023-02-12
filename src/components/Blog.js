1e06b7b7132d8030931deaeb52001f322d7cf1a4
DeepmalaDixit <117112464+DeepmalaDixit@users.noreply.github.com>
2023-02-12 14:17:06 +0530

    Update Blog.js

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -10,8 +10,8 @@ const Blog = () => {
             <h2>Checkout my awesome blogs </h2>
             <h3><a href='https://www.newtonschool.co/post/top-13-web-developer-interview-questions'>Web Development Interview questions</a></h3>
             <h3><a href='https://www.newtonschool.co/post/graph-data-structure-explained-with-examples'>Graph Data Structures</a></h3>
-            <button id="blog-to-info"> Go to Info</button>
-            <button id="blog-to-home"> Go to Home</button>
+            <button id="blog-to-info"> <Link to="/info">Go to Info</Link></button>
+            <button id="blog-to-home"> <Link to="/">Go to Home</Link></button>
         </div>
     )
 }
