b660b555ea55af398019bcc3de8a825d12af5ec9
DeepmalaDixit <117112464+DeepmalaDixit@users.noreply.github.com>
2023-02-12 14:30:20 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
             <h2>Hi, my name is Siddharth </h2>
             <h2>and I work at <a href="https://www.newtonschool.co/">Newton School</a></h2>
            <button id="home-to-blog"> <Link to="/blog">Go to Blog</Link></button>
-            <button id="home-to-info"> <Link to=:"/Info">Go to Info</Link></button>
+            <button id="home-to-info"> <Link to="/Info">Go to Info</Link></button>
 
         </div>
     )
