07df5e673de562c9b10ca8286922aeb7ae2a7989
DeepmalaDixit <117112464+DeepmalaDixit@users.noreply.github.com>
2023-02-12 14:21:13 +0530

    Update Info.js

--- src/components/Info.js
+++ src/components/Info.js
@@ -11,8 +11,8 @@ const Info = () => {
             <h1> Info page </h1>
             <h2>I am born in India</h2>
             <h2>and I like to play Cricket </h2>
-             <button id="info-to-blog"> Go to Blog</button>
-            <button id="info-to-home"> Go to Home</button>
+             <button id="info-to-blog"> <Link to="/blog">Go to Blog</Link></button>
+            <button id="info-to-home"> <Link to="/">Go to Home<Link></button>
         </div>
     )
 }
